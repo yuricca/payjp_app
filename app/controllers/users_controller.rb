aa224d4fc81545605c60ff795b929209d2815093
yuricca <yuricca531@gmail.com>
2021-10-03 13:27:11 +0900

    マイページの実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def show
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      redirect_to action: "show"
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+
+end
