fc2938242170e60da712af77113e1820ecabb78b
yuricca <yuricca531@gmail.com>
2021-10-03 13:01:59 +0900

    ログイン・トップページの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_perameters, if: :devise_controller?
+
+  private
+
+  def configure_perameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
